5ff5cab57eecd218dc2402b4aa6ca1507866c472
filipe.soares <filipe.rocha.soares@gmail.com>
2021-02-26 21:49:12 -0300

    Added classe of domain Categoria

---
+++ src/main/java/com/modelagemConceitual/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package com.modelagemConceitual.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.modelagemConceitual.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+	@GetMapping
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+
+}
