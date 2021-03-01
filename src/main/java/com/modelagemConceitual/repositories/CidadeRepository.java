ca1ef10a754580742a674cb890ee47049b874ae1
filipe.soares <filipe.rocha.soares@gmail.com>
2021-03-01 14:08:18 -0300

    added estado, cidade and repositories

---
+++ src/main/java/com/modelagemConceitual/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.modelagemConceitual.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.modelagemConceitual.domain.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
---
+++ src/main/java/com/modelagemConceitual/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.modelagemConceitual.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.modelagemConceitual.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
